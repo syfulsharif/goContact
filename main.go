ebe81ce09aba66676d633bd43c0bef70fe659e91
Syful Islam Sharif <sharif.syful@gmail.com>
2025-06-11 12:49:07 +0600

    Getting contact list loop updated

--- main.go
+++ main.go
@@ -1,6 +1,32 @@
 package main
 
-func main(){
-	// Map Struct 
-	
+import (
+	"fmt"
+	"github.com/syfulsharif/goContact/contact"
+)
+
+func main() {
+	// Map Struct
+	cm := contact.NewManager()
+	cm.AddContact(contact.Contact{
+		Name:  "John Doe",
+		Phone: "+88018868288",
+		Email: "jd@email.com",
+	})
+
+	cm.AddContact(contact.Contact{
+		Name:  "Fun Doe",
+		Phone: "+88018868388",
+		Email: "fd@email.com",
+	})
+
+	cm.AddContact(contact.Contact{
+		Name:  "Ron Doe",
+		Phone: "+88018868488",
+		Email: "rd@email.com",
+	})
+
+	for index, c := range cm.ContactList() {
+		fmt.Printf("Contact %d: %v\n", index, c)
+	}
 }
